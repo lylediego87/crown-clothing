ff885a831e44c42f6faca1c807710e0bd4e1e623
Lyle <lylediego87@gmail.com>
2021-04-15 20:35:18 +0200

    Create shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-
-
-function Hats() {
-  return (
-    <div><h1>Hats Page</h1></div>
-  );
-}
+import ShopPage from './pages/shoppage/shop.component';
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/hats' component={Hats} />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
